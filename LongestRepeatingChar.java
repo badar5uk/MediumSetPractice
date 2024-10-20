7f51fe9e16552af26b1c974a972acdbefb0a061a
bader <bader5uk@hotmail.com>
2024-10-20 13:17:55 +0400

    Fixed findLongestCharRepl function and initialized a final integer to represent alphabet size

--- LongestRepeatingChar.java
+++ LongestRepeatingChar.java
@@ -10,53 +10,33 @@ public class LongestRepeatingChar {
              - Use a for loop to go through each letter
              - use an integer k as the number of changes to be made
              - take a counter to keep track of the letters
-
-
-
-Hint: Use a HashSet to keep track of the characters you have seen and maintain a
-count of the maximum frequency of any character in the current window.
-
      */
-   static Set<List<Character>> setOfChar = new HashSet<>();
-   static List<Character> listOfChar = new ArrayList<>();
 
     public static void main(String[] args) {
         String originalString = "AABBBBA";
-        System.out.println("longest string that can be formed from" +"\n"+"the replacement of the string "+originalString + " is: "+findLongRepCharRepl(originalString,1));
+        System.out.println("longest string that can be formed from" + "\n" + "the replacement of the string " + originalString + " is: " + findLongRepCharRepl(originalString, 1));
 
     }
 
-    public static int findLongRepCharRepl(String s, Integer k) {
+    private static final Integer ALPHABET_SIZE = 26;
 
-        int[] counts_char = new int[26];
-        Set<Character> charIteration = new HashSet<>();
+    public static int findLongRepCharRepl(String s, int k) {
 
-        Integer j =0;
-        Integer maxCounter = 0;
-        Integer result = 0;
-        Integer counter = 0;
+        int[] countsChar = new int[ALPHABET_SIZE];
+        int j = 0, maxCounter = 0, maxLength = 0;
 
-        for(Integer i =0; i < s.length(); i++){
-            Character currentChar = s.charAt(i);
-            Integer windowSize = i - j + 1;
-            charIteration.add(currentChar);
+        for (int i = 0; i < s.length(); i++) {
+            countsChar[s.charAt(i) - 'A']++;
+            maxCounter = Math.max(maxCounter, countsChar[s.charAt(i) - 'A']);
 
-            counter = counts_char[s.charAt(i) - 'A']++;
-            counts_char[s.charAt(i) - 'A']++;
-
-            maxCounter = Math.max(maxCounter, counter);
-
-
-            Integer tempCounter = windowSize - maxCounter;
-
-            if(tempCounter > k){
-                counts_char[s.charAt(j) - 'A'] = counts_char[s.charAt(j) - 'A'] - 1;
-                j = j + 1;
-            }else{
-                result = Math.max(result,windowSize - 1 );
+            while (i - j + 1 - maxCounter > k) {
+                countsChar[s.charAt(j) - 'A']--;
+                j++;
             }
+
+            maxLength = Math.max(maxLength, i - j + 1);
         }
 
-        return  result;
+        return maxLength;
     }
 }
